911527b1cb40478398812dbf96b7af328ad9efa4
ltduc <lethaiduc11@gmail.com>
2022-09-05 18:55:48 +0700

    update authen

--- src/pages/LogoutPage.vue
+++ src/pages/LogoutPage.vue
@@ -2,10 +2,10 @@
   <div>Logout</div>
 </template>
 <script>
-import mapping from '@/store/mapping';
+// import mapping from '@/store/mapping';
 export default {
   methods: {
-    logoutAction: mapping.auth.actions.logout,
+    // logoutAction: mapping.auth.actions.logout,
   },
   mounted() {
     // this.logoutAction();
